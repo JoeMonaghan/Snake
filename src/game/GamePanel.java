830841ebd1948da081a261024923301e3357cc42
JoeMonaghan <joe@josephs-MacBook-Pro.local>
2018-03-19 22:26:27 +0000

    JFrame created and building successfuly.

--- src/game/GamePanel.java
+++ src/game/GamePanel.java
@@ -14,6 +14,9 @@ public class GamePanel extends JPanel implements Runnable, KeyListener {
 
 	public GamePanel() {
 		setPreferredSize(new Dimension(WIDTH, HEIGHT));
+		setFocusable(true);
+		requestFocus();
+		addKeyListener(this);
 		
 	}
 	
--- src/game/Main.java
+++ src/game/Main.java
@@ -1,10 +1,21 @@
 package game;
 
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		JFrame frame = new JFrame("Snake");
+		frame.setContentPane(new GamePanel());
+		frame.pack();
+		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+		frame.setResizable(false);
+		frame.setPreferredSize(new Dimension(GamePanel.WIDTH, GamePanel.HEIGHT));
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
 	}
 
 }
