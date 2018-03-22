133973fec27f92810efd96dfbe32f4c7b0880cef
JoeMonaghan <joe@josephs-MacBook-Pro.local>
2018-03-22 20:30:28 +0000

    Score label added to screen and apple rendering to screen.

--- src/game/GamePanel.java
+++ src/game/GamePanel.java
@@ -31,14 +31,13 @@ public class GamePanel extends JPanel implements Runnable, KeyListener {
 	Entity head;
 	ArrayList<Entity> snake;
 	private final int SIZE = 10;
-	
+
 	// movement
 	private int dx, dy;
-	
+
 	// key input
 	private boolean up, down, right, left, start;
-	
-	
+
 	public GamePanel() {
 		setPreferredSize(new Dimension(WIDTH, HEIGHT));
 		setFocusable(true);
@@ -70,34 +69,34 @@ public class GamePanel extends JPanel implements Runnable, KeyListener {
 	@Override
 	public void keyPressed(KeyEvent e) {
 		int k = e.getKeyCode();
-		
-		if(k == KeyEvent.VK_UP) 
+
+		if (k == KeyEvent.VK_UP)
 			up = true;
-		if(k == KeyEvent.VK_DOWN) 
+		if (k == KeyEvent.VK_DOWN)
 			down = true;
-		if(k == KeyEvent.VK_LEFT) 
+		if (k == KeyEvent.VK_LEFT)
 			left = true;
-		if(k == KeyEvent.VK_RIGHT) 
+		if (k == KeyEvent.VK_RIGHT)
 			right = true;
-		if(k == KeyEvent.VK_ENTER) 
+		if (k == KeyEvent.VK_ENTER)
 			start = true;
-		
+
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
 		int k = e.getKeyCode();
-		
-		if(k == KeyEvent.VK_UP) 
+
+		if (k == KeyEvent.VK_UP)
 			up = true;
-		if(k == KeyEvent.VK_DOWN) 
+		if (k == KeyEvent.VK_DOWN)
 			down = true;
-		if(k == KeyEvent.VK_LEFT) 
+		if (k == KeyEvent.VK_LEFT)
 			left = true;
-		if(k == KeyEvent.VK_RIGHT) 
+		if (k == KeyEvent.VK_RIGHT)
 			right = true;
-		if(k == KeyEvent.VK_ENTER) 
+		if (k == KeyEvent.VK_ENTER)
 			start = true;
 
 	}
@@ -133,19 +132,19 @@ public class GamePanel extends JPanel implements Runnable, KeyListener {
 	private void init() {
 		image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_ARGB);
 		g2d = image.createGraphics();
-		
+
 		running = true;
 		setUpLevel();
 		setFPS(10);
 
 	}
-	
+
 	private void setUpLevel() {
 		snake = new ArrayList<Entity>();
 		head = new Entity(SIZE);
 		head.setPosition(WIDTH / 2, HEIGHT / 2);
 		snake.add(head);
-		for(int i = 1; i < 10; i++) {
+		for (int i = 1; i < 10; i++) {
 			Entity e = new Entity(SIZE);
 			e.setPosition(head.getX() + (i * SIZE), head.getY());
 			snake.add(e);
@@ -155,53 +154,54 @@ public class GamePanel extends JPanel implements Runnable, KeyListener {
 	private void requestRender() {
 		render(g2d);
 		Graphics g = getGraphics();
-		g.drawImage(image, 0,0, null);
+		g.drawImage(image, 0, 0, null);
 		g.dispose();
 	}
 
 	private void update() {
-		if(up && dy == 0) {
+		if (up && dy == 0) {
 			dy = -SIZE;
 			dx = 0;
 		}
-		
-		if(down && dy == 0) {
+
+		if (down && dy == 0) {
 			dy = -SIZE;
 			dx = 0;
 		}
-		if(left && dx == 0) {
+		if (left && dx == 0) {
 			dy = 0;
 			dx = -SIZE;
 		}
-		if(right && dx == 0) {
+		if (right && dx == 0) {
 			dy = 0;
 			dx = -SIZE;
 		}
-		for(int i = snake.size() - 1; i>0 ; i--) {
-			snake.get(i).setPosition(
-					snake.get(i -1).getX(),
-					snake.get(i- 1).getY());
+
+		if (dx != 0 || dy != 0) {
+			for (int i = snake.size() - 1; i > 0; i--) {
+				snake.get(i).setPosition(snake.get(i - 1).getX(), snake.get(i - 1).getY());
+			}
+
+			head.move(dx, dy);
+
 		}
-		
-		head.move(dx, dy);
-		
-		if(head.getX() < 0)
+
+		if (head.getX() < 0)
 			head.setX(WIDTH);
-		if(head.getY() < 0)
+		if (head.getY() < 0)
 			head.setY(HEIGHT);
-		if(head.getX() >  WIDTH)
+		if (head.getX() > WIDTH)
 			head.setX(0);
-		if(head.getY() > HEIGHT)
+		if (head.getY() > HEIGHT)
 			head.setY(0);
-		
 
 	}
 
 	public void render(Graphics2D g2d) {
 		g2d.clearRect(0, 0, WIDTH, HEIGHT);
 		g2d.setColor(Color.GREEN);
-		
-		for(Entity e : snake) {
+
+		for (Entity e : snake) {
 			e.render(g2d);
 		}
 	}
